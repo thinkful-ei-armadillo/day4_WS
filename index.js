e55b046b6878ff768fcc8c422f6abd624fdc1ae4
William Y Wong <wwong1987@gmail.com>
2019-01-17 13:49:12 -0800

    working new item submit

--- index.js
+++ index.js
@@ -1,33 +1,59 @@
 'use strict';
 /* global $ */
 
-// create array of groceries?
 
+// create array of items??
+// traverse shopping list & manipulate items on event clicks
+// create a function to add an item to shopping list in input field
+// trigger event with that new item submission to add to list
+// check button needs to add strikethrough to item
+// delete button has to delete shopping list item 
 
-$(function() {
-  $('#shopping-list-form').submit(event => {
+function newItemSubmit() {
+  $('#js-shopping-list-form').submit(event => {
     event.preventDefault();
-    const userEntry = $(event.currentTarget).find('#shopping-list-entry');
-    $('.shopping-list').text(`user text is: ${userEntry.val()}`);
-    userEntry.val('');
+    const userEntry = $(event.currentTarget).find('#shopping-list-entry').val();
+    $('.shopping-list').append(`<li> 
+        <span class="shopping-item">${userEntry}</span> 
+        <div class="shopping-item-controls"> 
+        <button class="shopping-item-toggle"> 
+            <span class="button-label">check</span></button> 
+            <button class="shopping-item-delete"> <span class="button-label">delete</span> </button> 
+            </div></li>`);
+    // userEntry.val('');
+    console.log(userEntry);
   });
-});
+};
+
+
+newItemSubmit();
+
+
+
 
 console.log('hello world');
 
 
 // $('button')
-
 // use .submit(), preventDefault(), toggleClass(), closest()
 
+// $('button').click(function() { 
+//     let item = $('#shopping-list').val(); 
+//     $('#shopping-list').append( <li> 
+//         <span class="shopping-item">${userEntry}</span> 
+//         <div class="shopping-item-controls"> 
+//         <button class="shopping-item-toggle"> 
+//             <span class="button-label">check</span></button> 
+//             <button class="shopping-item-delete"> <span class="button-label">delete</span> </button> 
+//             </div></li>) });
 
 
-$('button').click(function() { 
-    let item = $('#shopping-list').val(); 
-    $('#shopping-list').append( <li> 
-        <span class="shopping-item">${item}</span> 
-        <div class="shopping-item-controls"> 
-        <button class="shopping-item-toggle"> 
-            <span class="button-label">check</span></button> 
-            <button class="shopping-item-delete"> <span class="button-label">delete</span> </button> 
-            </div></li>) });
+
+// function removeItemFromList(item) {
+//     $('.shopping-list-delete').click(function()
+//         { 
+//         const deletedItem= $('.shopping-list-item').val(); 
+//         ${deletedItem}.remove(); 
+//         })
+//     }
+
